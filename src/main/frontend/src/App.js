d9a62566d6783f0044c2d004606c35e9d4479a46
sying012 <syi369@naver.com>
2022-08-24 13:51:00 +0900

    220824-1

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,14 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowseRouter, Routes, Route } from "react-router-dom";
-import Layout from "./layout/Layout";
+import Layout from "./layouts/Layout";
 
 function App() {
-  return (
-    <Layout>
-      
-    </Layout>
-  ) 
+  return <Layout></Layout>;
 }
 
 export default App;
---
+++ src/main/frontend/src/layouts/Header.js
@@ -0,0 +1,29 @@
+import "../styles/layouts/Header.css";
+import whitelogo1 from "../images/logo_white1.png";
+import { useNavigate } from "react-router";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const toSearchCert = () => {
+    navigate("/");
+  };
+
+  return (
+    <header className="header">
+      <img className="whitelogo1" src={whitelogo1} alt="" />
+      <nav>
+        <p>자격증 찾기</p>
+        <p>세미나</p>
+        <p>마을회관</p>
+        <p>공지사항</p>
+      </nav>
+      <div>
+        <p>로그인</p>
+        <p>회원가입</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

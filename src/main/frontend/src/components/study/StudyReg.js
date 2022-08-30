9e5854843c066766be1a2cf80eae18aec2e67dbb
sying012 <syi369@naver.com>
2022-08-30 15:31:22 +0900

    Revert "Revert "스터디 글쓰기 미리보기 추가""
    This reverts commit d4eb9d9dd24c4c063b49a4d0d8fad6158c0da02b.

---
+++ src/main/frontend/src/components/study/StudyReg.js
@@ -0,0 +1,168 @@
+import React, { useRef } from "react";
+import "../../styles/study/StudyReg.css";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+
+const StudyReg = () => {
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 3.8 + ITEM_PADDING_TOP,
+        width: 120,
+      },
+    },
+  };
+
+  const [maxMemberCnt, setMaxMemberCnt] = React.useState("");
+
+  const handleChange = (event) => {
+    setMaxMemberCnt(event.target.value);
+  };
+
+  const menuItemList = () => {
+    const result = [];
+    for (let index = 1; index < 11; index++) {
+      result.push(
+        <MenuItem key={index} value={index}>
+          {index}명
+        </MenuItem>
+      );
+    }
+    return result;
+  };
+
+  const defaultContentValue = `  스터디 모집글을 아래 양식을 참고해 작성해주세요. 
+  꼼꼼히 작성하면 멋진 스터디 팀원을 만나실 수 있을거예요. 
+  
+  [스터디 모집 내용 예시]
+  
+  스터디 주제 :
+  스터디 목표 : 
+  예상 스터디 일정(횟수) :
+  예상 커리큘럼 간략히 :
+  예상 모집인원 :
+  스터디 소개 : 
+  스터디 주의사항 : 
+  스터디에 지원할 수 있는 방법을 남겨주세요. (이메일, 카카오 오픈채팅방, 구글폼 등.)`;
+
+  const readImage = (file) => {
+    // 인풋 태그에 파일이 있는 경우
+    if (file) {
+      // FileReader 인스턴스 생성
+      const reader = new FileReader();
+
+      // 이미지가 로드가 된 경우
+      reader.onload = (e) => {
+        const preImg = document.getElementById("studyImgPreview");
+        preImg.src = e.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div className="studyRegContainer">
+      <div className="leftContainer">
+        <img
+          className="studyImgPreview"
+          src="https://velog.velcdn.com/images/kshired/post/d8a48a1f-4106-480f-8307-d20eae1f9486/image.png"
+          alt="미리보기"
+          id="studyImgPreview"
+          onClick={() => {
+            document.getElementById("uploadImg").click();
+          }}
+        ></img>
+        <div className="selectorWrapper">
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="maxSelectLabel">최대인원</InputLabel>
+              <Select
+                labelId="maxSelectLabel"
+                id="maxSelect"
+                value={maxMemberCnt}
+                label="MaxMemberCnt"
+                onChange={handleChange}
+                MenuProps={MenuProps}
+              >
+                {menuItemList()}
+              </Select>
+            </FormControl>
+          </Box>
+        </div>
+        <input
+          hidden
+          type="file"
+          className="uploadImg"
+          name="uploadFiles"
+          multiple="multiple"
+          id="uploadImg"
+          onChange={(e) => {
+            readImage(e.target.files[0]);
+          }}
+        ></input>
+      </div>
+      <div className="rightContainer">
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "750px" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField id="studyTitleInput" label="제목" variant="outlined" />
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "750px" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="studyContentInput"
+            label="내용"
+            multiline
+            rows={15}
+            defaultValue={defaultContentValue}
+          />
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "750px" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="studyContactInput"
+            label="연락수단"
+            variant="outlined"
+          />
+        </Box>
+        <div className="submitBtnWrapper">
+          <button type="submit">등록</button>
+          <button
+            type="button"
+            onClick={() => {
+              window.location = "../study";
+            }}
+          >
+            취소
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StudyReg;
--- src/main/frontend/src/pages/CommunityMain.js
+++ src/main/frontend/src/pages/CommunityMain.js
@@ -8,16 +8,19 @@ import "../styles/community/CommunityMain.css";
 const CommunityMain = () => {
   const [studyList, setStudyList] = useState([
     {
-      studyId: 1,
+      id: 1,
       studyTitle: "스터디 제목1",
+      userId: "소방 안전관리 대장",
+      regDate: "2022.08.29",
+      contact: "http://open.kakao/a/dd0dd",
       studyContent: "어쩌고 저쩌고1",
-      studyMemCnt: 7,
+      studyMemCnt: 3,
       studyImg:
         "https://velog.velcdn.com/images/kshired/post/d8a48a1f-4106-480f-8307-d20eae1f9486/image.png",
       studyState: 0,
     },
     {
-      studyId: 2,
+      id: 2,
       studyTitle: "스터디 제목2",
       studyContent: "어쩌고 저쩌고2",
       studyMemCnt: 1,
@@ -25,12 +28,33 @@ const CommunityMain = () => {
       studyState: 1,
     },
     {
-      studyId: 3,
+      id: 3,
       studyTitle: "스터디 제목3",
       studyContent: "어쩌고 저쩌고3",
       studyMemCnt: 5,
       studyImg:
         "https://i0.wp.com/wowtale.net/wp-content/uploads/2019/08/%E1%84%80%E1%85%AE%E1%84%85%E1%85%AE%E1%84%86%E1%85%B5%E1%84%8F%E1%85%A2%E1%86%B7%E1%84%89%E1%85%B3%E1%84%90%E1%85%A5%E1%84%83%E1%85%B5-%E1%84%8B%E1%85%B5%E1%84%8B%E1%85%AD%E1%86%BC-%E1%84%8C%E1%85%A1%E1%86%BC%E1%84%86%E1%85%A7%E1%86%AB.png?w=1920&ssl=1",
+      studyState: 0,
+    },
+    {
+      id: 4,
+      studyTitle: "스터디 제목4",
+      studyContent: "어쩌고 저쩌고4",
+      studyMemCnt: 5,
+      studyImg:
+        "https://dimg.donga.com/wps/NEWS/IMAGE/2020/03/02/99972437.1.jpg",
+      studyState: 0,
+    },
+    {
+      id: 5,
+      studyTitle: "스터디 제목1",
+      userId: "소방 안전관리 대장",
+      regDate: "2022.08.29",
+      contact: "http://open.kakao/a/dd0dd",
+      studyContent: "어쩌고 저쩌고1",
+      studyMemCnt: 3,
+      studyImg:
+        "https://velog.velcdn.com/images/kshired/post/d8a48a1f-4106-480f-8307-d20eae1f9486/image.png",
       studyState: 0,
     },
   ]);

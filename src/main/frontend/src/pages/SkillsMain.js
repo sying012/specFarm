ff4484afae576bc6b7cd3e168c9d074a46f19480
awesomekimn <awesomekimn@gmail.com>
2022-09-03 12:36:18 +0900

    skillsMain, courseDetail 완성

--- src/main/frontend/src/pages/SkillsMain.js
+++ src/main/frontend/src/pages/SkillsMain.js
@@ -1,13 +1,61 @@
-import { Link } from "react-router-dom";
 import styles from "../styles/skills/SkillsMain.module.css";
 
 function SkillsMain() {
   return (
     <div>
       <div className={styles.mainContainer}>
-        <Link to="/skills/jobcafe">일자리 카페 바로가기</Link>
-        <br />
-        <Link to="/skills/findcourse">직업훈련탐색 바로가기</Link>
+        <div className={styles.subContainer}>
+          <a href="/skills/jobcafe">
+            <h1 className={styles.skillsTitle}>일자리 카페</h1>
+          </a>
+          <p className={styles.skillsSubTitle}>
+            일자리 카페에 대한 다양한 정보를 확인하세요.
+          </p>
+          <div className={styles.skillsSmallBtn}>
+            <div className={styles.skillsBtns}>
+              <a href="/skills/jobcafe">지역별</a>
+            </div>
+            <div className={styles.skillsBtns}>
+              <a href="/skills/jobcafe">취업 상담</a>
+            </div>
+            <div className={styles.skillsBtns}>
+              <a href="/skills/jobcafe">AI 모의면접</a>
+            </div>
+          </div>
+          <div className={styles.skillsImgBox1}>
+            <img
+              src="/upload/skills/improve.png"
+              alt="grawth"
+              className={styles.growthImg}
+            />
+          </div>
+        </div>
+        <div className={styles.subContainer}>
+          <a href="/skills/findcourse">
+            <h1 className={styles.skillsTitle}>직업훈련탐색</h1>
+          </a>
+          <p className={styles.skillsSubTitle}>
+            직업훈련에 대한 다양한 정보를 확인하세요.
+          </p>
+          <div className={styles.skillsSmallBtn}>
+            <div className={styles.skillsBtns}>
+              <a href="/skills/findcourse">지역별</a>
+            </div>
+            <div className={styles.skillsBtns}>
+              <a href="/skills/findcourse">직종별</a>
+            </div>
+            <div className={styles.skillsBtns}>
+              <a href="/skills/findcourse">분야별</a>
+            </div>
+          </div>
+          <div className={styles.skillsImgBox2}>
+            <img
+              src="/upload/skills/growth.png"
+              alt="grawth"
+              className={styles.growthImg}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );

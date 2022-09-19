d36e03eea681e29855c3c9258df778ebac495695
Sung Munsik <moon9508@google.com>
2022-09-19 16:40:43 +0900

    자격증목록 db저장

---
+++ src/main/java/com/spring/specfarm/dto/CertDTO.java
@@ -0,0 +1,29 @@
+package com.spring.specfarm.dto;
+
+import lombok.Data;
+
+@Data
+
+public class CertDTO {
+	
+	private String obligfldcd;
+	
+	private String seriescd;
+	
+	private String jmcd;
+	
+	private String mdobligfldnm;
+	
+	private String seriesnm;
+	
+	private String mdobligfldcd;
+	
+	private String obligfldnm;
+	
+	private String qualgbcd;
+	
+	private String CertName;
+		
+	private String qualgbnm;
+	
+}

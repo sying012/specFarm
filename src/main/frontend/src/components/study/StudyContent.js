14f24079010d602ffc937b457ff8a2cdee62860f
sying012 <syi369@naver.com>
2022-09-20 11:17:56 +0900

    스터디 상세 동기화처리, history, cookie 설치

--- src/main/frontend/src/components/study/StudyContent.js
+++ src/main/frontend/src/components/study/StudyContent.js
@@ -4,27 +4,13 @@ import { useNavigate, useParams } from "react-router";
 import styles from "../../styles/study/StudyContent.module.css";
 import { API_BASE_URL } from "../../app-config";
 
-const StudyContent = ({ studyList, setStudyList }) => {
+const StudyContent = ({}) => {
   const { id } = useParams();
   const [toggleList, setToggleList] = useState(0);
   const [requestState, setRequestState] = useState(0);
   const [loginUserId, setLoginUserId] = useState("");
-  const study = studyList[studyList.length - id];
-  // const [studyss, setStudyss] = useState(null);
-  // console.log(studyList);
-
-  const {
-    studyTitle,
-    user,
-    regDate,
-    contact,
-    studyContent,
-    studyMemberCnt,
-    studyImgName,
-    studyYn,
-    studyMaxMember,
-    studyIdx,
-  } = study;
+  // const study = studyList[studyList.length - id];
+  const [study, setStudy] = useState();
 
   useEffect(() => {
     axios
@@ -54,8 +40,7 @@ const StudyContent = ({ studyList, setStudyList }) => {
       params: { id: studyIdx },
     })
       .then((response) => {
-        console.log(response);
-        setStudyList(response.data.studyList.content);
+        // console.log(response);
         navigate(`..`);
       })
       .catch((e) => {
@@ -63,70 +48,41 @@ const StudyContent = ({ studyList, setStudyList }) => {
       });
   };
 
-  // useEffect(() => {
-  //   axios
-  //     .get(API_BASE_URL + "/community/study/getStudy", {
-  //       headers: {
-  //         Authorization: "Bearer " + sessionStorage.getItem("ACCESS_TOKEN"),
-  //       },
-  //       params: { id: id },
-  //     })
-  //     .then((response) => {
-  //       console.log(response.data.study);
-  //       setStudyss(response.data.study);
-  //       console.log(studyss);
-  //     })
-  //     .catch((e) => {
-  //       console.log(e.data.error);
-  //     });
-  // }, []);
+  useEffect(() => {
+    axios
+      .get(API_BASE_URL + "/community/study/getStudy", {
+        headers: {
+          Authorization: "Bearer " + sessionStorage.getItem("ACCESS_TOKEN"),
+        },
+        params: { id: id },
+      })
+      .then((response) => {
+        console.log(response.data.study);
+        setStudy(response.data.study);
+        console.log(study);
+      })
+      .catch((e) => {
+        console.log(e.data.error);
+      });
+  }, [study]);
 
   return (
-    <div className={styles.studyContent}>
-      <div className={styles.contentContainer}>
-        <div className={styles.contentLeft}>
-          <img
-            className={styles.studyBanner}
-            src={"/upload/study/" + studyImgName}
-            alt="스터디 배너"
-          ></img>
-          <div className={styles.joinListWrapper}>
-            <div className={styles.listName}>
-              <p>신청자 목록</p>
-              <p className={styles.memberCnt}>{studyMemberCnt}명 신청중</p>
-            </div>
-            <div className={styles.joinList}>
-              <div className={styles.participant}>
-                <img
-                  src="/upload/study/ms.png"
-                  className={styles.partImg}
-                  alt="참가자 프로필사진"
-                ></img>
-                <p className={styles.partId}>소방대장</p>
-                {user.userId === loginUserId ? (
-                  <button className={styles.byebtn} type="button">
-                    어서와
-                  </button>
-                ) : (
-                  ""
-                )}
+    study !== undefined && (
+      <div className={styles.studyContent}>
+        <div className={styles.contentContainer}>
+          <div className={styles.contentLeft}>
+            <img
+              className={styles.studyBanner}
+              src={"/upload/study/" + study.studyImgName}
+              alt="스터디 배너"
+            ></img>
+            <div className={styles.joinListWrapper}>
+              <div className={styles.listName}>
+                <p>신청자 목록</p>
+                <p className={styles.memberCnt}>
+                  {study.studyMemberCnt}명 신청중
+                </p>
               </div>
-            </div>
-            <div
-              className={styles.joinListToggle}
-              style={
-                toggleList
-                  ? { borderRadius: "0" }
-                  : { borderRadius: "0 0 4px 4px" }
-              }
-              onClick={() => {
-                setToggleList(!toggleList);
-              }}
-            >
-              <p>참여자 목록</p>
-              <p className={styles.memberCnt}>{studyMemberCnt}명 참여중</p>
-            </div>
-            {toggleList ? (
               <div className={styles.joinList}>
                 <div className={styles.participant}>
                   <img
@@ -134,110 +90,145 @@ const StudyContent = ({ studyList, setStudyList }) => {
                     className={styles.partImg}
                     alt="참가자 프로필사진"
                   ></img>
-                  <p className={styles.partId}>{user.userId}</p>
-                  <img
-                    className={styles.badge}
-                    src="/upload/study/crown.png"
-                    alt="방장표시"
-                  ></img>
-                </div>
-                <div className={styles.participant}>
-                  <img
-                    src="/upload/study/ms.png"
-                    className={styles.partImg}
-                    alt="참가자 프로필사진"
-                  ></img>
                   <p className={styles.partId}>소방대장</p>
-                  {user.userId === loginUserId ? (
+                  {study.user.userId === loginUserId ? (
                     <button className={styles.byebtn} type="button">
-                      잘가
+                      어서와
                     </button>
                   ) : (
                     ""
                   )}
                 </div>
               </div>
-            ) : (
-              ""
-            )}
-          </div>
-        </div>
-        <div className={styles.contentRight}>
-          <div className={styles.titleWrapper}>
-            <div
-              className={styles.studyContentState}
-              style={{
-                color: "white",
-                background: studyYn === "Y" ? "#1d5902" : "#8cbf75",
-              }}
-              onClick={() => {
-                // user.userId === loginUserId ?  :
-              }}
-            >
-              {studyYn === "Y" ? "모집" : "완료"}
+              <div
+                className={styles.joinListToggle}
+                style={
+                  toggleList
+                    ? { borderRadius: "0" }
+                    : { borderRadius: "0 0 4px 4px" }
+                }
+                onClick={() => {
+                  setToggleList(!toggleList);
+                }}
+              >
+                <p>참여자 목록</p>
+                <p className={styles.memberCnt}>
+                  {study.studyMemberCnt}명 참여중
+                </p>
+              </div>
+              {toggleList ? (
+                <div className={styles.joinList}>
+                  <div className={styles.participant}>
+                    <img
+                      src="/upload/study/ms.png"
+                      className={styles.partImg}
+                      alt="참가자 프로필사진"
+                    ></img>
+                    <p className={styles.partId}>{study.user.userId}</p>
+                    <img
+                      className={styles.badge}
+                      src="/upload/study/crown.png"
+                      alt="방장표시"
+                    ></img>
+                  </div>
+                  <div className={styles.participant}>
+                    <img
+                      src="/upload/study/ms.png"
+                      className={styles.partImg}
+                      alt="참가자 프로필사진"
+                    ></img>
+                    <p className={styles.partId}>소방대장</p>
+                    {study.user.userId === loginUserId ? (
+                      <button className={styles.byebtn} type="button">
+                        잘가
+                      </button>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
-            <p className={styles.studyTitle}>{studyTitle}</p>
           </div>
-          <div className={styles.makerNdate}>
-            <div className={styles.maker}>
-              <img
-                src="/upload/study/ms.png"
-                className={styles.partImg}
-                alt="참가자 프로필사진"
-              ></img>
-              <p className={styles.studyMaker}>{user.userId}</p>
+          <div className={styles.contentRight}>
+            <div className={styles.titleWrapper}>
+              <div
+                className={styles.studyContentState}
+                style={{
+                  color: "white",
+                  background: study.studyYn === "Y" ? "#1d5902" : "#8cbf75",
+                }}
+                onClick={() => {
+                  // user.userId === loginUserId ?  :
+                }}
+              >
+                {study.studyYn === "Y" ? "모집" : "완료"}
+              </div>
+              <p className={styles.studyTitle}>{study.studyTitle}</p>
+            </div>
+            <div className={styles.makerNdate}>
+              <div className={styles.maker}>
+                <img
+                  src="/upload/study/ms.png"
+                  className={styles.partImg}
+                  alt="참가자 프로필사진"
+                ></img>
+                <p className={styles.studyMaker}>{study.user.userId}</p>
+              </div>
+
+              <p className={styles.studyRegDate}>{study.regDate}</p>
+            </div>
+            <div className={styles.contactWrapper}>
+              <p>연락수단 👉</p>
+              <a href="#" className={styles.contact}>
+                {study.contact}
+              </a>
+            </div>
+            <div className={styles.contentWrapper}>
+              <pre className={styles.content}>{study.studyContent}</pre>
             </div>
 
-            <p className={styles.studyRegDate}>{regDate}</p>
-          </div>
-          <div className={styles.contactWrapper}>
-            <p>연락수단 👉</p>
-            <a href="#" className={styles.contact}>
-              {contact}
-            </a>
-          </div>
-          <div className={styles.contentWrapper}>
-            <pre className={styles.content}>{studyContent}</pre>
-          </div>
-
-          <div className={styles.btnWrapper}>
-            {user.userId === loginUserId ? (
-              <>
-                <button
-                  type="button"
-                  onClick={() => {}}
-                  className={styles.reqBtnStyle}
-                  style={{ marginRight: "10px" }}
-                >
-                  수정
-                </button>
+            <div className={styles.btnWrapper}>
+              {study.user.userId === loginUserId ? (
+                <>
+                  <button
+                    type="button"
+                    onClick={() => {}}
+                    className={styles.reqBtnStyle}
+                    style={{ marginRight: "10px" }}
+                  >
+                    수정
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      deleteStudy(study.studyIdx);
+                    }}
+                    className={styles.cancelBtnStyle}
+                  >
+                    삭제
+                  </button>
+                </>
+              ) : (
                 <button
                   type="button"
                   onClick={() => {
-                    deleteStudy(studyIdx);
+                    setRequestState(!requestState);
                   }}
-                  className={styles.cancelBtnStyle}
+                  className={
+                    requestState ? styles.reqBtnStyle : styles.cancelBtnStyle
+                  }
                 >
-                  삭제
+                  {requestState ? "취소" : "신청"}
                 </button>
-              </>
-            ) : (
-              <button
-                type="button"
-                onClick={() => {
-                  setRequestState(!requestState);
-                }}
-                className={
-                  requestState ? styles.reqBtnStyle : styles.cancelBtnStyle
-                }
-              >
-                {requestState ? "취소" : "신청"}
-              </button>
-            )}
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
